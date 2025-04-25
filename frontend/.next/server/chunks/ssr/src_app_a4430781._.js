422b12c0aa9069f0be484410fabbb0c13257917b
Luca Aguiar <lucanobre1@gmail.com>
2025-04-25 18:26:42 -0300

    icon header

--- frontend/.next/server/chunks/ssr/src_app_a4430781._.js
+++ frontend/.next/server/chunks/ssr/src_app_a4430781._.js
@@ -4,7 +4,7 @@ module.exports = {
 
 var { g: global, __dirname } = __turbopack_context__;
 {
-__turbopack_context__.v("/_next/static/media/favicon.a1f001f3.ico");}}),
+__turbopack_context__.v("/_next/static/media/favicon.2f6bf76f.ico");}}),
 "[project]/src/app/favicon.ico.mjs { IMAGE => \"[project]/src/app/favicon.ico (static in ecmascript)\" } [app-rsc] (structured image object, ecmascript)": ((__turbopack_context__) => {
 "use strict";
 
